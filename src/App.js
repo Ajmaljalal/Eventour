66ceb23e8c00e450fefcb6ea413e31ab7e8848de
Ajmal Jalal <ajmaljalal58@gmail.com>
2018-09-26 16:20:24 -0700

    added pricing, location, footer components and scroll to feature

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
+import {Element} from 'react-scroll';
+
 import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
 import EventInfo from './components/venueInfo';
 import Highlights from './components/highlights';
-import Pricing from './components/highlights/pricing';
+import Pricing from './components/pricing';
+import Location from './components/location';
+import Footer from './components/header_footer/Footer';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{height: '1500px'}}>
-        <Header/>
-        <Featured/>
-        <EventInfo/>
-        <Highlights/>
-        <Pricing/>
+        <Element>
+          <Header/>
+        </Element>
+        <Element name='eventStartsIn'><Featured/></Element>
+        <Element name='eventInfo'><EventInfo/></Element>
+        <Element name='highlights'><Highlights/></Element>
+        <Element name='pricing'><Pricing/></Element>
+        <Element name='location'><Location/></Element>
+        <Footer />
       </div>
     );
   }
