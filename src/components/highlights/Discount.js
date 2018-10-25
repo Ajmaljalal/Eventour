2607c863a05e5e618eb3d4cf2c460eadb5b44d60
Ajmal Jalal <ajmaljalal58@gmail.com>
2018-10-25 00:00:00 -0700

    added the pricing componet

--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
+import CustomButton from '../utils/CustomButton';
 
 class Discount extends Component {
     state = {
@@ -38,6 +39,7 @@ class Discount extends Component {
                         <div className='discount_description'>
                             <h3>Purchase tickets before 20th Oct</h3>
                             <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+                            <CustomButton title='Purchase now' href='https://www.google.com'/>
                         </div> 
                     </Slide>
 
