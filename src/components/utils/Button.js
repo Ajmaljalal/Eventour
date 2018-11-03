7bad7f203bcc5d1328c17cbe805a09932ee6d730
Ajmal Jalal <ajmaljalal58@gmail.com>
2018-11-03 00:00:00 -0700

    added Higlights section

---
+++ src/components/utils/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import ticketIcon from '../../resources/images/icons/ticket.png'
+
+const Button = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Button;
